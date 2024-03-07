b748b454037d12107fcb51fdf8560092e7034715
Ankur2903 <sainiankur0508@gmail.com>
2024-03-07 13:27:48 +0530

    ReactProject

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,10 +14,10 @@ export default function Navbar(props) {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        <a className="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link active" href="/about">about</a>
+                        <a className="nav-link active" href="#">about</a>
                     </li>
                 </ul>
                 {/* <form className="d-flex" role="search">
